ae5adfbd4b7b70351eebe7fd647c0081544bad4b
Vera177 <veradeviaje@gmail.com>
2021-09-05 00:14:30 -0300

    mmmm dale

--- js/favourites.js
+++ js/favourites.js
@@ -3,40 +3,43 @@ import { superSizeRender, downloadFunction } from "./modules/renderView.js";
 // Función cargar LocalStorage desde la array vacía. addEventListener sobre el botón me gusta. Eso lo agrega a una array vacía, que se va a pushear y de ahí cargar al LocalStorage
 //Otra función para renderizar.
 let apiKey = 'np4xYBCqbTJh3AtzJOzmHPfPPOJoafpg';
-let favouriteGifss = [];
+let favouriteGifsOriginal = [];
+let favouriteGifs = [];
 let gifFavourites = document.getElementById('gifFavourites');
 let noFavGifs = document.getElementById('noFavGifs');
 let showMore = document.getElementById('showMore');
 
 export async function getImagess() {
 
-    favouriteGifss = JSON.parse(localStorage.getItem('favourites'));
-    if (!favouriteGifss) {
+    favouriteGifsOriginal = JSON.parse(localStorage.getItem('favourites'));
+    favouriteGifs = new Set (favouriteGifsOriginal);
+    console.log("original", favouriteGifsOriginal, "sin repes", favouriteGifs);
+    if (!favouriteGifs) {
         noFavAdded();
     } else {
         //forma1
 
         // for (let i = 0; i < JSON.parse(localStorage.getItem('favourites')).length; i++) {
-        //     favouriteGifss.push(JSON.parse(localStorage.getItem('favourites'))[i]);
+        //     favouriteGifs.push(JSON.parse(localStorage.getItem('favourites'))[i]);
         // }
         // for (let i = 0; i < JSON.parse(localStorage.getItem('favouriteFromSearch')).length; i++) {
-        //     favouriteGifss.push(JSON.parse(localStorage.getItem('favouriteFromSearch'))[i]);
+        //     favouriteGifs.push(JSON.parse(localStorage.getItem('favouriteFromSearch'))[i]);
         // }
 
         //forma 1a (original)
 
-        // favouriteGifss = JSON.parse(localStorage.getItem('favourites')) || JSON.parse(localStorage.getItem('favouriteFromSearch'));
-        // favouriteGifss += JSON.parse(localStorage.getItem('favouriteFromSearch'));
+        // favouriteGifs = JSON.parse(localStorage.getItem('favourites')) || JSON.parse(localStorage.getItem('favouriteFromSearch'));
+        // favouriteGifs += JSON.parse(localStorage.getItem('favouriteFromSearch'));
 
         //forma2
 
-        // favouriteGifss = JSON.parse(localStorage.getItem('favourites')).concat(JSON.parse(localStorage.getItem('favouriteFromSearch')))
+        // favouriteGifs = JSON.parse(localStorage.getItem('favourites')).concat(JSON.parse(localStorage.getItem('favouriteFromSearch')))
 
         // concat = función avanzada de js para concatenar arreglos.
 
         gifFavourites.innerHTML = '';
 
-        let response = await fetch(`https://api.giphy.com/v1/gifs?ids=${favouriteGifss}&api_key=${apiKey}`);
+        let response = await fetch(`https://api.giphy.com/v1/gifs?ids=${favouriteGifs}&api_key=${apiKey}`);
         let infoMyGif = await response.json();
         renderViewFav(infoMyGif);
     }
@@ -52,7 +55,7 @@ function noFavAdded() {
 function renderViewFav(infoMyGif) {
     let gifFavourites = document.getElementById('gifFavourites');
     let cardGifo;
-    for (let i = 0; i < favouriteGifss.length; i++) {
+    for (let i = 0; i < favouriteGifs.length; i++) {
         cardGifo = document.createElement('li');
         cardGifo.className = 'cardGifo';
         cardGifo.innerHTML = `<img src="${infoMyGif.data[i].images.original.url}" alt="imagen gif">
@@ -74,10 +77,10 @@ function renderViewFav(infoMyGif) {
 
         let trash = document.getElementById(`trash_${[i]}`);
         trash.addEventListener('click', () => {
-            favouriteGifss.splice(i, 1);
-            localStorage.setItem('favourites', JSON.stringify(favouriteGifss));
+            favouriteGifs.splice(i, 1);
+            localStorage.setItem('favourites', JSON.stringify(favouriteGifs));
             getImagess();
-            // localStorage.removeItem('favourites', JSON.stringify(favouriteGifss[i]));
+            // localStorage.removeItem('favourites', JSON.stringify(favouriteGifs[i]));
         });
 
         //Download function
@@ -92,3 +95,12 @@ function renderViewFav(infoMyGif) {
     showMore.style.display = 'block';
 }
 
+let arr = [1,2,3,3,4,5,5,5,5,5];
+
+let unicos = new Set(arr);
+
+console.log("Unicos: ", [...unicos]);
+
+let repes = ["hola", "hola", "Manuela", "eugenia"];
+let unicoss = new Set(repes);
+console.log(unicoss);
